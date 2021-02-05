42c4e7fff156a8de024c6b6bc51726ec0f5a9d59
ymasog <yer.masog@gmail.com>
2021-02-04 23:43:29 -0600

    added values for employee table

--- employeeSeed.sql
+++ employeeSeed.sql
@@ -13,7 +13,7 @@ CREATE TABLE department (
   
 );
 INSERT INTO department (id, name)
-VALUES (101, "HR");
+VALUES (101, "administration");
 
 INSERT INTO department (id, name)
 VALUES (102, "sales");
@@ -36,7 +36,10 @@ CREATE TABLE role (
 );
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES ("201", "human resources", 50000, 101);
+VALUES ("201", "receptionist", 50000, 101);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES ("206", "human resources", 50000, 101);
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES ("202", "accountant", 50000, 103);
@@ -45,10 +48,15 @@ INSERT INTO role (id, title, salary, department_id)
 VALUES ("203", "customer service rep", 50000, 104);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES ("204", "salesperson", 60000, 102);
+VALUES ("207", "quality assurance", 50000, 104);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES ("204", "sales rep", 60000, 102);
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES ("205", "delivery", 50000, 105);
+
+
 
 
 CREATE TABLE employee (
@@ -58,20 +66,67 @@ CREATE TABLE employee (
   role_id INTEGER(11),
   manager_id INTEGER(11)
 );
+--Michael 999
+--Angela 888
+--Darryl 777
 
 -- Creates new rows containing data in all named columns --
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES ("741", "Dwight", "Schrute", 222, 999);
+VALUES (999, "Michael", "Scott", 204);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES ("Ahmed", TRUE, "Rockington", 100);
+VALUES (741, "Dwight", "Schrute", 204, 999);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES ("Jacob", TRUE, "Misty", 10);
+VALUES (456, "Jim", "Halpert", 204, 999);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES ("Jacob", TRUE, "Misty", 10);
+VALUES (321, "Andy", "Bernard", 204, 999);
 
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (369, "Stanley", "Hudson", 204, 999);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (852, "Phyllis", "Vance", 204, 999);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (821, "Ryan", "Howard", 204, 999);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (888, "Angela", "Martin", 202);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (159, "Oscar", "Martinez", 202, 888);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (123, "Kevin", "Malone", 202, 888);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (777, "Darryl", "Philbin", 205);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (357, "Glen", "X", 205, 777);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (486, "Lonny", "Collins", 205, 777);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (258, "Madge", "Madsen", 205, 777);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (951, "Kelly", "Kapoor", 203, 999);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (526, "Meredith", "Palmer", 203, 999);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (788, "Creed", "Bratton", 207, 999);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (294, "Pam", "Beesly", 201, 999);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (666, "Toby", "Flenderson", 206, 999);
 
 select * from employee
 
